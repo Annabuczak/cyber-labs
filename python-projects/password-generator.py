699ed85241e3cc7bafe2fb9d15cee5e1bd60ac88
Anna Buczak <ab25014@essex.ac.uk>
2026-06-20 11:12:24 +0100

    Finish interactive password generator

--- python-projects/password-generator.py
+++ python-projects/password-generator.py
@@ -1,17 +1,34 @@
 import random
-import string
 
-letters = "abcde"
-numbers = "1,2,3,4,5,6,7,8,9,0"
-symbols = "!@£$%^&*()_+-=<>?,./?;:"
-print(random.choice(letters))
-print(random.choice(numbers))
-print(random.choice(symbols))
+random_letters = "abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+random_numbers = "1234567890"
+random_symbols = "!@£$%^&*()_+-=<>?,./?;:"
+
+
+password_length = int(input("How long do you want your password to be? "))
+
+letters_choice = input("Do you want to use random letters y/n? ").lower()
+if letters_choice == "y":
+    letters = random_letters
+else:
+    letters = input("Enter the letters you want to use: ")
+
+numbers_choice = input("Do you want to use random numbers y/n? ").lower()
+if numbers_choice == "y":
+    numbers = random_numbers
+else:
+    numbers = input("Enter the numbers you want to use: ")
+
+symbols_choice = input("Do you want to use random symbols y/n? ").lower()
+if symbols_choice == "y":
+    symbols = random_symbols
+else:
+    symbols = input("Enter the symbols you want to use: ")
+
+characters = letters + numbers + symbols
 
 password = ""
-for _ in range(12):
-    password += random.choice(letters)
-    password += random.choice(symbols)
-    password += random.choice(numbers)
+for _ in range(password_length):
+    password += random.choice(characters)
 
 print(password)
