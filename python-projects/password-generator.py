1aff93e942267960c8bfb660105aa3a3e50e6da7
Anna Buczak <ab25014@essex.ac.uk>
2026-06-20 07:40:12 +0100

    Add password generator project

---
+++ python-projects/password-generator.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+letters = "abcde"
+numbers = "1,2,3,4,5,6,7,8,9,0"
+symbols = "!@£$%^&*()_+-=<>?,./?;:"
+print(random.choice(letters))
+print(random.choice(numbers))
+print(random.choice(symbols))
+
+password = ""
+for _ in range(12):
+    password += random.choice(letters)
+    password += random.choice(symbols)
+    password += random.choice(numbers)
+
+print(password)
+
+
+
