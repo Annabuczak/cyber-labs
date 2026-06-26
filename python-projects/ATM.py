7d8e68b2fe901091f13102c3c48a78e9c88fe23c
Anna Buczak <ab25014@essex.ac.uk>
2026-06-26 19:02:45 +0100

    p

---
+++ python-projects/ATM.py
@@ -0,0 +1,19 @@
+correct_pin = "1234"
+attempt_left = 2
+
+user_pin = input("Please enter your PIN: ")
+
+if user_pin == correct_pin:
+    print("Access granted.")
+else:
+    print("Incorrect PIN.")
+
+while attempt_left >0:
+    user_pin = input("Please enter your PIN: ")
+    if user_pin == correct_pin:
+        print("Access granted.")
+    else:
+        print("Incorrect PIN.")
+        attempt_left -= 1
+        print("Card is blocked. Contact provider")
+        break
