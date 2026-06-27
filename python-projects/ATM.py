e4c8b18daba36ec92b83551241655ace9b9cfd9d
Anna Buczak <ab25014@essex.ac.uk>
2026-06-27 13:29:05 +0100

    ww

--- python-projects/ATM.py
+++ python-projects/ATM.py
@@ -1,4 +1,6 @@
 print("Welcome to Bank of Anna.")
+
+
 correct_pin = "1234"
 attempt_left = 3
 
@@ -15,6 +17,16 @@ while attempt_left > 0:
         if attempt_left == 0:
             print("Card is blocked. Contact provider")
 
+class InvalidAmountError(Exception):
+    pass
+
+class InvalidAmountError(Exception):
+    pass
+
+
+class InsufficientFundsError(Exception):
+    pass
+
 
 class CurrentAccount:
     def __init__(self, owner, balance,correct_pin):
@@ -22,8 +34,8 @@ class CurrentAccount:
         self.balance = balance
         self.correct_pin = correct_pin
 
-    def deposit(self):
-        amount = int(input("Enter amount to deposit: "))
+    def deposit(self,amount):
+
         self.balance += amount
         if amount <= 0:
             print("Incorrect amount.")
@@ -32,32 +44,61 @@ class CurrentAccount:
         return amount
 
 
-    def withdraw(self):
-        amount = int(input("How much would you like to withdraw? "))
-        if amount > self.balance:
+    def withdraw(self,amount):
+        if amount > 0:
             print("You don't have enough money!")
-            return 0
+            raise InsufficientFundsError("Not enough funds.")
+
         else:
             self.balance -= amount
             print(f"Withdrew £{amount} successfully!")
             return amount
 
     def check_balance(self):
-        return self.balance
-
+        if self.balance <= 0:
+            print("Current balance!")
+            return 0
+        else:
+           print(self.balance)
 
 owner = "Louie"
 start_balance = 0
 account = CurrentAccount(owner, start_balance,correct_pin)
 
-deposited_amount = account.deposit()
-withdrawn_amount = account.withdraw()
+
 balance = account.check_balance()
 
-print(
-    f"{owner}'s balance is £{balance}. "
-    f"{owner} has deposited £{deposited_amount}. "
-    f"{owner} has withdrawn £{withdrawn_amount}."
-)
 
 
+
+while True:
+    print("\n===== Bank of Anna =====")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Check balance")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+    try:
+
+        if choice == "1":
+           amount = int(input("Enter amount to deposit: "))
+           account.deposit(amount)
+    except ValueError:
+        print("Please enter numbers.")
+        continue
+    try:
+        if choice == "2":
+           amount = int(input("Enter amount to withdraw: "))
+           account.withdraw(amount)
+    except ValueError:
+        print("Please enter numbers.")
+        continue
+
+    if choice == "3":
+        account.check_balance()
+    elif choice == "4":
+        print("Thank you for using Bank of Anna. Goodbye")
+    else:
+        print("Goodbye.")
+        break
