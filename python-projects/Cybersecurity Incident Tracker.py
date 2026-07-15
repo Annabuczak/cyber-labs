ade977cdb3a9986b35bf97543113d3285cb3a67c
Anna Buczak <ab25014@essex.ac.uk>
2026-07-15 16:34:17 +0100

    ww

--- python-projects/Cybersecurity Incident Tracker.py
+++ python-projects/Cybersecurity Incident Tracker.py
@@ -4,3 +4,22 @@
 # Staff can report incidents, analysts can investigate them, evidence can be added,
 # and each case can move through different statuses until it is closed.
 
+class Reporter:
+    def __init__(self,name,email,password,department):
+        self.name = name
+        self.email = email
+        self.password = password
+
+
+class Analyst(Reporter):
+    def __init__(self, name, email, password, department, skill_level):
+        super().__init__(name, email, password, department)
+        self.skill_level = skill_level
+
+analyst_1 = Analyst(
+    "Anna",
+    "anna@email.com",
+    "password123",
+    "Cybersecurity",
+    "Beginner"
+)
